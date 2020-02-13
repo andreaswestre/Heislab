534e69d6b46d76f98c1b65fa864111f2eafd5ae0
akselva <https://akselva@github.com>
2020-02-13 17:22:41 +0100

    laget floor sensor logic

---
+++ source/floor_sensor_logic.c
@@ -0,0 +1,8 @@
+//
+//  floor_sensor_logic.c
+//  
+//
+//  Created by Aksel Vaaler on 13/02/2020.
+//
+
+#include "floor_sensor_logic.h"
---
+++ source/floor_sensor_logic.h
@@ -0,0 +1,13 @@
+//
+//  floor_sensor_logic.h
+//  
+//
+//  Created by Aksel Vaaler on 13/02/2020.
+//
+
+#ifndef floor_sensor_logic_h
+#define floor_sensor_logic_h
+
+#include <stdio.h>
+
+#endif /* floor_sensor_logic_h */
