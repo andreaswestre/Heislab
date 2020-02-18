c137d93a522d764b0c67d9777efaf2e5ed545f30
akselva <https://akselva@github.com>
2020-02-18 12:02:22 +0100

    endringer

--- source/queue_logic.c
+++ source/queue_logic.c
@@ -6,3 +6,8 @@
 //
 
 #include "queue_logic.h"
+
+static order_type * order_array = order_array = malloc(4*sizeof(order_type));
+static order_type * next_floor_pointer = order_array+1;
+static order_type * end_floor_pointer = order_array;
+
--- source/queue_logic.h
+++ source/queue_logic.h
@@ -18,8 +18,7 @@ typedef enum{
 static order_type * order_array;
 
 
-order_array = malloc(4*sizeof(order_type));
-static order_type * next_floor_pointer = order_array+1;
-static order_type * end_floor_pointer = order_array;
-void manage_order_array(order_type * next_floor_pointer,order_type )
+static order_type * next_floor_pointer;
+static order_type * end_floor_pointer;
+void manage_order_array(order_type * next_floor_pointer,order_type);
 #endif /* queue_logic_h */
