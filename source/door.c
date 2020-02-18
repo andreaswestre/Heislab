a393c977dc858b01510b2f6fd89c0eac3fa1e0ff
andreaswestre <andw@stud.ntnu.no>
2020-02-18 13:34:38 +0100

    Door logic

--- source/door.c
+++ source/door.c
@@ -7,6 +7,41 @@
 
 #include "door.h"
 
+void setTimeout()
+{
+    int milliseconds = 3000;
+    // If milliseconds is less or equal to 0
+    // will be simple return from function without throw error
+    if (milliseconds <= 0) {
+        fprintf(stderr, "Count milliseconds for timeout is less or equal to 0\n");
+        return;
+    }
+    
+    // a current time of milliseconds
+    int milliseconds_since = clock() * 1000 / CLOCKS_PER_SEC;
+    
+    // needed count milliseconds of return from this timeout
+    int end = milliseconds_since + milliseconds;
+    
+    // wait while until needed time comes
+    do {
+        milliseconds_since = clock() * 1000 / CLOCKS_PER_SEC;
+        if (milliseconds_since % 1000 == 0 && print == 1){
+            print = 0;
+            //printf("1 sec");
+        }
+        print = 1;
+    } while (milliseconds_since <= end);
+    
+}
+
+void open_door(){
+    printf("Door is open");
+    setTimeout();
+    printf("Door is closed");
+    
+    
+}
 
 
  
--- source/door.h
+++ source/door.h
@@ -11,6 +11,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void setTimeout();
+
+void open_door();
 
 
 
