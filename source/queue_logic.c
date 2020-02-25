651f057b924ebd9f4dc2fc06dd3b69e66ed54544
akselva <https://akselva@github.com>
2020-02-25 12:16:31 +0100

    masse endringer

--- source/queue_logic.c
+++ source/queue_logic.c
@@ -13,12 +13,12 @@ order_status order_array[4];
 int end_floor = 0;
 int * end_floor_pointer = &end_floor;
 
-void set_end_floor(int *end_floor_pointer, order_status *order_pointer, int current_floor){
+void set_end_floor(int *end_floor_pointer, order_status *order_array_pointer, int current_floor){
     for(int i = 0; i<4; i++){
 
-        if((order_pointer[i].UP==1) ||
-           (order_pointer[i].DOWN==1) ||
-           (order_pointer[i].OUT==1)){
+        if((order_array_pointer[i].UP==1) ||
+           (order_array_pointer[i].DOWN==1) ||
+           (order_array_pointer[i].INSIDE==1)){
             if((i>*end_floor_pointer) &&
                (*end_floor_pointer>=current_floor)){
                 *end_floor_pointer = i;
@@ -31,36 +31,35 @@ void set_end_floor(int *end_floor_pointer, order_status *order_pointer, int curr
     }
 }
 
-void add_orders(order_status *add_order_pointer){
+void add_orders(order_status *order_array_pointer){
     for (int i = 0; i<4; i++) {
         if(hardware_read_order(i,HARDWARE_ORDER_UP)){
-            add_order_pointer[i].UP = 1;
+            order_array_pointer[i].UP = 1;
            // printf("Order UP: %d", i);
         }
         if(hardware_read_order(i,HARDWARE_ORDER_DOWN)){
-            add_order_pointer[i].DOWN = 1;
+            order_array_pointer[i].DOWN = 1;
             //printf("Order DOWN: %d", i);
 
         }
         if(hardware_read_order(i,HARDWARE_ORDER_INSIDE)){
-            add_order_pointer[i].OUT = 1;
-            //printf("Order OUT: %d", i);
+            order_array_pointer[i].INSIDE = 1;
+            //printf("Order INSIDE: %d", i);
 
         }
     }
-
-
+    //set_order_lights(order_array);
 }
 
 
-int stop_or_continue(int current_floor,int end_floor, order_status *order_pointer){//Returns 1 if the elevator should stop
-    if((order_pointer[current_floor].UP==1) && (end_floor>=current_floor)){
+int stop_or_continue(int current_floor,int end_floor, order_status *order_array_pointer){//Returns 1 if the elevator should stop
+    if((order_array_pointer[current_floor].UP==1) && (end_floor>=current_floor)){
         return 1;
     }
-    else if((order_pointer[current_floor].DOWN==1) && (end_floor<=current_floor)){
+    else if((order_array_pointer[current_floor].DOWN==1) && (end_floor<=current_floor)){
         return 1;
     }
-    else if(order_pointer[current_floor].OUT==1){
+    else if(order_array_pointer[current_floor].INSIDE==1){
         return 1;
     }
     else{
@@ -68,8 +67,16 @@ int stop_or_continue(int current_floor,int end_floor, order_status *order_pointe
     }
 }
 
-void remove_orders(int current_floor, order_status *order_pointer){
-    order_pointer[current_floor].UP = 0;
-    order_pointer[current_floor].DOWN = 0;
-    order_pointer[current_floor].OUT = 0;
+void remove_orders(int current_floor, order_status *order_array_pointer){
+    order_array_pointer[current_floor].UP = 0;
+    order_array_pointer[current_floor].DOWN = 0;
+    order_array_pointer[current_floor].INSIDE = 0;
 }
+
+/*void set_order_lights(order_status order_array){
+    for(int i = 0; i<4; i++){
+        hardware_command_order_light[i,HARDWARE_ORDER_UP,order_array[i].UP];
+        hardware_command_order_light[i,HARDWARE_ORDER_DOWN,order_array[i].DOWN];
+        hardware_command_order_light[i,HARDWARE_ORDER_INSIDE,order_array[i].INSIDE];
+    }
+}*/
--- source/queue_logic.h
+++ source/queue_logic.h
@@ -11,7 +11,7 @@
 typedef struct{
     unsigned int UP;
     unsigned int DOWN;
-    unsigned int OUT;
+    unsigned int INSIDE;
 }order_status;
 
 /**
@@ -31,33 +31,33 @@ int * end_floor_pointer;
 
 /**
  *@brief adds orders to @c order_array.
- *@param add_order_pointer pointer to @c order_status.
+ *@param order_array_pointer pointer to @c order_status.
  */
-void add_orders(order_status *add_order_pointer);
+void add_orders(order_status *order_array_pointer);
 
 /**
  *@brief Updates @c end_floor.
  *@param end_floor_pointer pointer to the current end floor.
- *@param order_pointer pointer to the order array.
+ *@param order_array_pointer pointer to the order array.
  *@param current_floor The current floor.
  */
-void set_end_floor(int *end_floor_pointer, order_status *order_pointer,  int current_floor);
+void set_end_floor(int *end_floor_pointer, order_status *order_array_pointer,  int current_floor);
 
 /**
  *@brief Stops the elevator on floors with current orders.
  *@param current_floor The current floor.
  *@param end_floor The current end floor.
- *@param order_pointer pointer to the order array.
+ *@param order_array_pointer pointer to the order array.
  */
-int stop_or_continue(int current_floor, int end_floor, order_status *order_pointer);
+int stop_or_continue(int current_floor, int end_floor, order_status *order_array_pointer);
 
 /**
  *@brief Removes orders on current floor if elevator has stopped.
  *@param current_floor The current floor
- *@param order_pointer pointer to the order array.
+ *@param order_array_pointer pointer to the order array.
  */
-void remove_orders(int current_floor, order_status *order_pointer);
+void remove_orders(int current_floor, order_status *order_array_pointer);
 
-void set_order_lights(order_status order_array);
+//void set_order_lights(order_status order_array);
 #endif /* queue_logic_h */
 
